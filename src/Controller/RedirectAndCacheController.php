3fff5cb8e236efb29d3569268abb32558ac8d343
Francesco <me@francescoallara.it>
2018-10-24 15:51:19 +0100

    Programmes 6639 redirect segments and suggestions from v2 to v3 (#790)

--- src/Controller/RedirectAndCacheController.php
+++ src/Controller/RedirectAndCacheController.php
@@ -3,13 +3,19 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RedirectAndCacheController extends BaseController
 {
-    public function redirectAction(Request $request, $route, UrlGeneratorInterface $router, $permanent = false, $ignoreAttributes = false)
-    {
+    public function redirectAction(
+        Request $request,
+        $route,
+        UrlGeneratorInterface $router,
+        $permanent = false,
+        $ignoreAttributes = false
+    ) {
         if ('' == $route) {
             throw new HttpException($permanent ? 410 : 404);
         }
@@ -23,7 +29,19 @@ class RedirectAndCacheController extends BaseController
             }
         }
 
-        $response = new RedirectResponse($router->generate($route, $attributes, UrlGeneratorInterface::ABSOLUTE_URL), $permanent ? 301 : 302);
+        if ($permanent) {
+            $statusCode = Response::HTTP_MOVED_PERMANENTLY;
+        } else {
+            /**
+             * If we change it to 307 we have to re-test all the shit, but we use this constant now
+             * to keep track of the fact that 302 changed its meaning
+             */
+            $statusCode = Response::HTTP_FOUND;
+        }
+        $response = new RedirectResponse(
+            $router->generate($route, $attributes, UrlGeneratorInterface::ABSOLUTE_URL),
+            $statusCode
+        );
         $response->setPublic()->setMaxAge(3600);
         
         return $response;
