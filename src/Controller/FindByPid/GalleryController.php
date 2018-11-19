83bc1c3e593d79f5abd62054b969c740813e350a
tomwinnington <37040275+tomwinnington@users.noreply.github.com>
2018-11-19 15:21:39 +0000

    Gallery page outline and queries (#824)

--- src/Controller/FindByPid/GalleryController.php
+++ src/Controller/FindByPid/GalleryController.php
@@ -4,16 +4,46 @@ namespace App\Controller\FindByPid;
 
 use App\Controller\BaseController;
 use BBC\ProgrammesPagesService\Domain\Entity\Gallery;
+use BBC\ProgrammesPagesService\Domain\Entity\Image;
+use BBC\ProgrammesPagesService\Service\ImagesService;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class GalleryController extends BaseController
 {
-    public function __invoke(Gallery $gallery)
-    {
+    public function __invoke(
+        Gallery $gallery,
+        ImagesService $imagesService,
+        ?string $imagePid
+    ) {
         $this->setIstatsProgsPageType('galleries_show');
         $this->setContextAndPreloadBranding($gallery);
 
-        return $this->renderWithChrome('find_by_pid/example_entity.html.twig', [
-            'group' => $gallery,
+        $images = $imagesService->findByGroup($gallery);
+        $image = $this->getFirstImage($imagePid, $images);
+        return $this->renderWithChrome('find_by_pid/gallery.html.twig', [
+            'gallery' => $gallery,
+            'image' => $image,
+            'images' => $images,
         ]);
+    }
+
+    public function getFirstImage(?string $imagePid, array $images): ?Image
+    {
+        if (empty($images)) {
+            return null;
+        }
+        if (!$imagePid) {
+            return reset($images);
+        }
+        $image = null;
+        foreach ($images as $eachImage) {
+            if (((string) $eachImage->getPid()) === $imagePid) {
+                $image = $eachImage;
+            }
+        }
+        if (!$image) {
+            throw new NotFoundHttpException('Image not found.');
+        }
+        return $image;
     }
 }
