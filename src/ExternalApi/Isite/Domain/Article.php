fac48d0c0ceafd62fe7e62ec716d2c4497203c20
Francesco Allara <francesco.allara@bbc.co.uk>
2018-12-03 16:01:22 +0000

    Sync with fixed history

--- src/ExternalApi/Isite/Domain/Article.php
+++ src/ExternalApi/Isite/Domain/Article.php
@@ -41,9 +41,6 @@ class Article
     /** @var RowGroup[] */
     private $rowGroups;
 
-    /** @var int */
-    private $childCount;
-
     public function __construct(
         string $title,
         string $key,
@@ -66,7 +63,6 @@ class Article
         $this->image = $image;
         $this->parents = $parents;
         $this->rowGroups = $rowGroups;
-        $this->setChildCount(0);
     }
 
     /**
@@ -156,10 +152,5 @@ class Article
     public function setChildren(array $children)
     {
         $this->children = $children;
-    }
-
-    public function setChildCount(int $childCount)
-    {
-        $this->childCount = $childCount;
     }
 }
